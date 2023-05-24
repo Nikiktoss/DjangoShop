a5357c273c1109a3db510349050af72b0be8ff10
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-05-24 20:13:45 +0300

    Add Order and OrderItem models

--- orderapp/admin.py
+++ orderapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created", "updated")
+    list_display_links = ("id", "user")
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "product")
+    list_display_links = ("id", "order")
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
--- orderapp/models.py
+++ orderapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from mainapp.models import Product
+from authapp.models import ShopUser
 
-# Create your models here.
+
+class Order(models.Model):
+    FORMING = "FM"
+    SENT_TO_PROCEED = "STP"
+    PROCEEDED = "PRD"
+    PAID = "PD"
+    READY = "RDY"
+    CANCEL = "CNC"
+
+    ORDER_STATUS_CHOICES = (
+        (FORMING, "forming"),
+        (SENT_TO_PROCEED, "sent for processing"),
+        (PAID, "paid"),
+        (PROCEEDED, "processed"),
+        (READY, "ready"),
+        (CANCEL, "canceled"),
+    )
+
+    user = models.ForeignKey(ShopUser, on_delete=models.CASCADE)
+    created = models.DateTimeField(verbose_name="Date of creation", auto_now_add=True)
+    updated = models.DateTimeField(verbose_name="Updated", auto_now=True)
+    status = models.CharField(verbose_name="status", max_length=3, choices=ORDER_STATUS_CHOICES, default=FORMING)
+
+    def __str__(self):
+        return f"{self.user.name}:{self.status}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name="order_items", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Product")
+    quantity = models.PositiveIntegerField(verbose_name="Quantity", default=1)
