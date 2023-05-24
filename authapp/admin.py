5325c746243f95c6d417c4f8d2a44fb4b4716837
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-05-24 20:11:31 +0300

    Add ShopUser and ShopUSerProfile models

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import ShopUser, ShopUserProfile
+
+
+admin.site.site_header = 'DjangoShop Administration'
+admin.site.site_title = 'DjangoShop admin site'
+
+
+class ShopUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "display_photo", "is_superuser")
+    list_display_links = ("id", "username")
+    readonly_fields = ("display_photo",)
+
+    def display_photo(self, obj):
+        if obj.photo:
+            return format_html(f'<img src="{obj.photo.url}" style="width: 80px; height: 80px"')
+        else:
+            return format_html(f'<img src="/static/default_images/default_user_photo.webp"'
+                               f' style="width: 80px; height: 80px"')
+
+
+class ShopUserProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "gender")
+    list_display_links = ("id", "user")
+
+
+admin.site.register(ShopUser, ShopUserAdmin)
+admin.site.register(ShopUserProfile, ShopUserProfileAdmin)
--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class ShopUser(AbstractUser):
+    photo = models.ImageField(upload_to='users_photo', verbose_name="Account photo", blank=True)
+    age = models.PositiveIntegerField(verbose_name="Age", default=18)
+
+
+class ShopUserProfile(models.Model):
+    MALE = "M"
+    FEMALE = "W"
+
+    gender_choices = ((MALE, "M"), (FEMALE, "W"))
+
+    user = models.OneToOneField(ShopUser, unique=True, verbose_name="User", null=False, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=1, verbose_name="Gender", choices=gender_choices, blank=True)
+    about_me = models.TextField(verbose_name="About me", max_length=512, blank=True)
+
+    @staticmethod
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @staticmethod
+    @receiver(post_save, sender=ShopUser)
+    def save_profile(sender, instance, created, **kwargs):
+        user = instance
+        if created:
+            profile = ShopUserProfile(user=user)
+            profile.save()
