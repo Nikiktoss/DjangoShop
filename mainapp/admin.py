aecfef25058a4bb9cfd5cae97d3b9b3c2fa1893e
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-05-24 20:13:18 +0300

    add Product and ProductCategory models

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, ProductCategory
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "quantity")
+    list_display_links = ("id", "name")
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductCategory, ProductCategoryAdmin)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=75, verbose_name="Name", unique=True)
+    description = models.TextField(max_length=500, verbose_name="Description")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Product category"
+        verbose_name_plural = "Product categories"
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=75, verbose_name="Name")
+    image = models.ImageField(upload_to=f'products_images/{category.name}', verbose_name="Image", blank=True)
+    description = models.TextField(max_length=500, verbose_name="Description", blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Price", default=0)
+    quantity = models.PositiveIntegerField(verbose_name="Quantity", default=0)
+
+    def __str__(self):
+        return f"{self.category.name}: {self.name}"
