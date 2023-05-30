b0820e8aab2ab09c19ca49950d64cc98844529d9
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-05-30 23:35:51 +0300

    Adding urls of mainapp and authapp

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('mainapp.urls', 'mainapp'), namespace='mainapp')),
+    path('auth/', include(('authapp.urls', 'authapp'), namespace='authapp')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
