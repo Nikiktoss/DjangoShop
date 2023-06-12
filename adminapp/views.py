6137aade76442fe32628fc21d4ad231ed2df5b5b
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-06-12 20:45:48 +0300

    add view of main page in adminapp

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from authapp.models import ShopUser
 
-# Create your views here.
+
+def check_admin(user):
+    return user.is_superuser
+
+
+@user_passes_test(check_admin)
+def main_admin(request):
+    users = ShopUser.objects.all()
+    return render(request, 'main_admin_page.html', context={'user': request.user, 'users': users})
